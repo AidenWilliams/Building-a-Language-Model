997cfe7270785274b1c8418d6b29f6e557e40780
Aiden <mw3dbest@gmail.com>
2021-04-17 16:09:47 +0200

    Need to implement sentence generation
    otherwise testing isnt being done properly

--- LanguageModel.py
+++ LanguageModel.py
@@ -6,16 +6,16 @@ from collections import defaultdict
 
 
 class Corpus(object):
-    def __init__(self, corpus=None, directory='Corpus/'):
+    def __init__(self, corpus=None, directory='Corpus/', verbose=False):
         if corpus is None:
-            self._corpus = self._CorpusAsListOfSentences(directory)
+            self._corpus = self.CorpusAsListOfSentences(directory, verbose)
         else:
             self._corpus = corpus
         identifier = tuple([1, 'vanilla'])
         self._ngrams = {}
-        self._ngrams[identifier] = self.NGram(n=1)
+        self._ngrams[identifier] = self.NGram(n=1, verbose=verbose)
         self._models = {}
-        self._models[identifier] = self.Model(n=1)
+        self._models[identifier] = self.Model(n=1, verbose=verbose)
 
     def __len__(self):
         return len(self._corpus)
@@ -29,31 +29,31 @@ class Corpus(object):
 
     # Create functions to free memory once function scope is left
     @staticmethod
-    def _ReadCorpus(root='Corpus/'):
+    def _ReadCorpus(root='Corpus/', verbose=False):
         if not os.access(root, os.R_OK):
             print('Check root!!')
 
         xml_data = []
 
-        for file in tqdm(os.listdir(root), desc='Reading Files'):
+        for file in tqdm(os.listdir(root), desc='Reading Files', disable=not verbose):
             xml_data.append(open(os.path.join(root, file), 'r', encoding='utf8').read())  # Read file
         return xml_data
 
     @staticmethod
-    def _ParseAsXML(root='Corpus/'):
+    def _ParseAsXML(root='Corpus/', verbose=False):
         parser = etree.XMLParser(recover=True)
         roots = []
-        xml_data = Corpus._ReadCorpus(root)
-        for xml in tqdm(xml_data, desc='Parsing XML'):
+        xml_data = Corpus._ReadCorpus(root, verbose)
+        for xml in tqdm(xml_data, desc='Parsing XML', disable=not verbose):
             roots.append(etree.fromstring(xml, parser=parser))
         return roots
 
     @staticmethod
-    def _CorpusAsListOfSentences(root='Corpus/'):
-        roots = Corpus._ParseAsXML(root)
+    def CorpusAsListOfSentences(root='Corpus/', verbose=False):
+        roots = Corpus._ParseAsXML(root, verbose)
         sentences = []
-        for root in tqdm(roots, desc='XML File'):
-            for i, p in tqdm(enumerate(root), desc='Paragraph'):
+        for root in tqdm(roots, desc='XML File', disable=not verbose):
+            for i, p in tqdm(enumerate(root), desc='Paragraph', disable=not verbose):
                 for k, s in enumerate(p):
                     unfiltered_sentence = re.split(r'\n', s.text.lstrip('\n'))
                     sentence = []
@@ -68,9 +68,9 @@ class Corpus(object):
                         sentence.append('</s>')
         return sentences
 
-    def Counts(self, n):
+    def Counts(self, n, verbose=False):
         counts = defaultdict(lambda: 0)
-        for s in tqdm(self, desc='Counting x counts'):
+        for s in tqdm(self, desc='Counting x counts', disable=not verbose):
             for i in range(len(s) + 1):
                 if i < n:
                     continue
@@ -83,7 +83,7 @@ class Corpus(object):
 
         return counts
 
-    def NGram(self, n=2, model='vanilla'):
+    def NGram(self, n=2, model='vanilla', verbose=False):
         if n < 1:
             raise Exception('Unigrams and up are supported, otherwise no.')
 
@@ -98,14 +98,14 @@ class Corpus(object):
                 return self._ngrams[identifier]
 
         if model == 'laplace' or model == 'vanilla':
-            counts = self.Counts(n=n)
+            counts = self.Counts(n, verbose)
 
             if model == 'laplace':
                 for x in counts:
                     counts[x] += 1
 
         else:
-            _count = self.Counts(n=1)
+            _count = self.Counts(1, verbose)
             tc = []
             for s in self:
                 ts = []
@@ -116,8 +116,8 @@ class Corpus(object):
                         ts.append(w)
                 tc.append(ts)
 
-            temp = Corpus(corpus=tc)
-            counts = temp.Counts(n=n)
+            temp = Corpus(corpus=tc, verbose=verbose)
+            counts = temp.Counts(n, verbose)
 
         result = {
             'count': counts,
@@ -127,7 +127,7 @@ class Corpus(object):
         self._ngrams[identifier] = result
         return self._ngrams[identifier]
 
-    def GetCount(self, sequence: tuple, model='vanilla'):
+    def GetCount(self, sequence: tuple, model='vanilla', verbose=False):
         n = len(sequence)
         if n < 1:
             raise Exception('Unigrams and up are supported, otherwise no.')
@@ -137,7 +137,7 @@ class Corpus(object):
                 model != 'unk':
             raise Exception('Only "vanilla"/"laplace"/"unk" models are supported.')
 
-        _ngram = self.NGram(n, model)['count']
+        _ngram = self.NGram(n, model, verbose)['count']
 
         if sequence in _ngram:
             return _ngram[sequence]
@@ -147,7 +147,7 @@ class Corpus(object):
             else:
                 return 0
 
-    def Model(self, n=2, model='vanilla'):
+    def Model(self, n=2, model='vanilla', verbose=False):
         if n < 1:
             raise Exception('Unigrams and up are supported, otherwise no.')
 
@@ -161,10 +161,10 @@ class Corpus(object):
             if self._models[identifier].model == model:
                 return self._models[identifier]
 
-        self._models[identifier] = Model(corpus=self, n=n, model=model)
+        self._models[identifier] = Model(corpus=self, n=n, model=model, verbose=model)
         return self._models[identifier]
 
-    def GetProbability(self, input, n, model='vanilla'):
+    def GetProbability(self, input, n, model='vanilla', verbose=False):
         if n < 1:
             raise Exception('Unigrams and up are supported, otherwise no.')
 
@@ -174,7 +174,7 @@ class Corpus(object):
             raise Exception('Only "vanilla"/"laplace"/"unk" models are supported.')
 
         if type(input) is str:
-            tc = Corpus([[input]])
+            tc = Corpus([[input]], verbose=verbose)
         else:
             paragraph = False
             for elem in input:
@@ -189,8 +189,8 @@ class Corpus(object):
             else:
                 tc = Corpus([input])
 
-        _model = self.Model(n=n, model=model)
-        _ngram = tc.NGram(n=n, model=model)
+        _model = self.Model(n=n, model=model, verbose=verbose)
+        _ngram = tc.NGram(n=n, model=model, verbose=verbose)
 
         input_probability = 1
         exists = False
@@ -203,7 +203,7 @@ class Corpus(object):
 
         return input_probability
 
-    def TrigramLinearInterpolation(self, input, model='vanilla'):
+    def TrigramLinearInterpolation(self, input, model='vanilla', verbose=False):
         if model != 'vanilla' and \
                 model != 'laplace' and \
                 model != 'unk':
@@ -218,23 +218,23 @@ class Corpus(object):
                                 'str]].')
 
         if paragraph:
-            tc = Corpus(input)
+            tc = Corpus(input, verbose=verbose)
         else:
-            tc = Corpus([input])
+            tc = Corpus([input], verbose=verbose)
 
         l1 = 0.1
         l2 = 0.3
         l3 = 0.6
 
-        _ngram = tc.NGram(n=3, model=model)
+        _ngram = tc.NGram(n=3, model=model, verbose=verbose)
 
         output = 1
         exists = False
         for _n in _ngram['count']:
             exists = True
-            output *= l3 * self.GetProbability(input=[_n[2], _n[0], _n[1]], n=3, model=model) + \
-                      l2 * self.GetProbability(input=[_n[2], _n[1]], n=2, model=model) + \
-                      l1 * self.GetProbability(input=_n[2], n=1, model=model)
+            output *= l3 * self.GetProbability(input=[_n[2], _n[0], _n[1]], n=3, model=model, verbose=verbose) + \
+                      l2 * self.GetProbability(input=[_n[2], _n[1]], n=2, model=model, verbose=verbose) + \
+                      l1 * self.GetProbability(input=_n[2], n=1, model=model, verbose=verbose)
 
         if not exists:
             output = 0
@@ -243,20 +243,20 @@ class Corpus(object):
 
 
 class Model(object):
-    def __init__(self, corpus, n=2, model='vanilla'):
+    def __init__(self, corpus, n=2, model='vanilla', verbose=False):
         V = 0
         cmodel = model
         if model == 'laplace':
             cmodel = 'vanilla'
-            V = len(corpus.NGram(n=1)['count'])
+            V = len(corpus.NGram(n=1, verbose=verbose)['count'])
 
-        counts = corpus.NGram(n, model=cmodel)['count']
+        counts = corpus.NGram(n, model=cmodel, verbose=verbose)['count']
 
         probabilities = {}
         self.N = len([w for s in corpus for w in s])
 
         if n is not 1:
-            previous = corpus.NGram(n - 1, model=cmodel)['count']
+            previous = corpus.NGram(n - 1, model=cmodel, verbose=verbose)['count']
             for x in counts:
                 probabilities[x] = {
                     'probability': (counts[x] + int(model == 'laplace')) / (previous[x[:n - 1]] + V)}
@@ -275,12 +275,29 @@ class Model(object):
         else:
             return 0
 
-    def Perplexity(self):
-        prob = 1
-        for p in self.probabilities:
-            prob *= self.probabilities[p]['probability']
-        if prob == 0:
+    def Perplexity(self, input, verbose=False):
+        paragraph = False
+        for elem in input:
+            if isinstance(elem, list):
+                paragraph = True
+            if paragraph and not isinstance(elem, list):
+                raise Exception('Input must be of the forms:\n[str, str, str]\n[[str, str, str], ...,  [str, str, '
+                                'str]].')
+
+        if paragraph:
+            tc = Corpus(input, verbose=verbose)
+        else:
+            tc = Corpus([input], verbose=verbose)
+
+        _ngram = tc.NGram(n=3, model=self.model, verbose=verbose)
+
+        output = 1
+        exists = False
+        for _n in _ngram['count']:
+            exists = True
+            output *= self.GetProbabilityMath(_n[2], (_n[0], _n[1]))
+
+        if not exists or output == 0:
             return 0
         else:
-            return prob ** -(1 / self.N)
-
+            return output ** -(1 / self.N)
